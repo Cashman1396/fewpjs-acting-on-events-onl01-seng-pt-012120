32a2d9afe05706a5e7634dd4c588d22ee28d058f
Chris Ashman <chrisashman96@gmail.com>
2020-07-15 21:36:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Your code here
 let dodger = document.getElementById("dodger");
 
-var dodger = document.getElementById('dodger');
-
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10)
